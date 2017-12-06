5802e355f944d32c82838eb7d4dbb958127fa9fc
jimmyhenriksson <jimmyhen@kth.se>
2017-12-06 17:41:52 +0100

    Fixade så att man kan starta spelet med btn1.

--- pong_new/pongpeng/main.c
+++ pong_new/pongpeng/main.c
@@ -5,16 +5,12 @@
 
 #define DISPLAY_CHANGE_TO_COMMAND_MODE (PORTFCLR = 0x10)
 #define DISPLAY_CHANGE_TO_DATA_MODE (PORTFSET = 0x10)
-
 #define DISPLAY_ACTIVATE_RESET (PORTGCLR = 0x200)
 #define DISPLAY_DO_NOT_RESET (PORTGSET = 0x200)
-
 #define DISPLAY_ACTIVATE_VDD (PORTFCLR = 0x40)
 #define DISPLAY_ACTIVATE_VBAT (PORTFCLR = 0x20)
-
 #define DISPLAY_TURN_OFF_VDD (PORTFSET = 0x40)
 #define DISPLAY_TURN_OFF_VBAT (PORTFSET = 0x20)
-
 
 #define MAX_X               127
 #define MAX_Y               31
@@ -227,19 +223,16 @@ void tick() {
     ball.x += ball.speedX;
     ball.y += ball.speedY;
 
-    // check if ball is on the bottom of the screen
     if (ball.y <= 0) {
         ball.y = 0;
         ball.speedY *= (-1);
     }
 
-    //top of screen
     else if (ball.y >= MAX_Y) {
         ball.y = MAX_Y;
         ball.speedY *= (-1);
     }
 
-    //if ball is on the far left side of the screen and touches player 1
     if (ball.x <= 2) {
         if ((ball.y >= player1.y) && (ball.y <= (player1.y + 8))){
             ball.x = 2;
@@ -250,7 +243,7 @@ void tick() {
             scored();
         }
     }
-        //if ball is on the right side of the screen and touches player 1
+
     if (ball.x >= (MAX_X - 2)) {
         if ((ball.y >= player2.y) && (ball.y <= (player2.y + 8))){
             ball.x = MAX_X-2;
@@ -300,78 +293,19 @@ void startGame(){
     player2.speedY = 0;
 }
 
-//we update a pixel into SPI format, so it can light up.
-//We have 4 rows (0,1,2,3), that consist of columns of 8 bits (8*4 = 32 = screen height)
-//we have 128 of these columns
-void updatePixel(int x, int y){
+void update_pixel(int x, int y){
     int row = 0;
     if(y>0) {
         row = y / 8;
     }
-    // the OR-masking gives us access to the bit in our column of size 8, that we want to turn on.
     font[row * 128 + x] |= 1 << (y - row * 8);
 }
 
-//draw out the player
 void drawPlayer(Player p) {
     int i, j;
-    //For the width
     for (i = 0; i < PLAYER_WIDTH; i++){
-        //for the height
         for (j = 0; j < PLAYER_HEIGHT; j++){
-            updatePixel(p.x + i, p.y + j);
-        }
-    }
-}
-
-
-void display_print(char *s, int x, int y)
-{
-    int cur_char, cur_slice, cur_col, cur_row;
-    int c_printed; /* number of characters printed */
-    char textbuffer[16];
-
-    /* Check if valid coordinates */
-    if(x < 0 || x > (127 - 8) || y < 0 || y > (31 - 8))
-        return;
-
-    /* Check if empty string */
-    if(!s)
-        return;
-
-    /* Copy string contents to text buffer */
-    for(cur_char = 0; cur_char < 16; cur_char++)
-    {
-        if(*s)
-        {
-            textbuffer[cur_char] = *s;
-            s++;
-        }
-        else
-        {
-            textbuffer[cur_char] = ' ';
-        }
-    }
-
-    /* Copy string to screen buffer */
-    for(c_printed = 0; c_printed < 16; c_printed++)
-    {
-        /* Get next ascii character to print */
-        cur_char = textbuffer[c_printed];
-
-        /* Copy bitmap data to screen buffer */
-        for(cur_col = 0; cur_col < 8; cur_col++)
-        {
-            /* Get 8 pixel high slice from character bitmap */
-            cur_slice = font[cur_char*8 + cur_col];
-
-            /* Copy current 8 pixel slice to screen buffer */
-            for(cur_row = 0; cur_row < 8; cur_row++)
-            {
-                /* If pixel in slice is set, then set pixel in buffer */
-                if( (cur_slice >> cur_row) & 0x1 )
-                    updatePixel(x+8*c_printed+cur_col, y+cur_row);
-            }
+            update_pixel(p.x + i, p.y + j);
         }
     }
 }
@@ -398,13 +332,13 @@ void drawBall(Ball b) {
     int i, j;
     for (i = 0; i < BALL_WIDTH; i++){
         for (j = 0; j < BALL_HEIGHT; j++){
-            updatePixel(b.x + i, b.y + j);
+            update_pixel(b.x + i, b.y + j);
         }
     }
 }
 
 
-void resetScreen(){
+void screen_reset(){
     int i;
     for(i = 0; i< (128*4); i++){
         font[i] = 0;
@@ -521,7 +455,7 @@ void display_update_pong(void){
 }
 
 void drawToScreen(){
-    resetScreen();
+    screen_reset();
     drawPlayer(player1);
     drawPlayer(player2);
     drawBall(ball);
@@ -575,12 +509,12 @@ int main(void) {
         j++;
     }
 
-    resetScreen();
+    screen_reset();
 
     while(i){
         display_update_pong();
         display_update();
-        if (getbtns() > 0) {
+        if ((getbtns() || buttonOne()) > 0) {
             i = 0;
         }
     };
@@ -592,10 +526,7 @@ int main(void) {
 
         }
         tick();
-
         drawToScreen();
     }
-
-    for(;;) ;
     return 0;
 }
